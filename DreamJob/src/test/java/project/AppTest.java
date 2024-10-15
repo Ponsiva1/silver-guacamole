f4d001942946e56f48f17470e7ab432c286567c2
Ponsiva1 <ponsiva97@gmail.com>
2024-10-15 18:45:12 +0530

    assertion learn commit

--- DreamJob/src/test/java/project/AppTest.java
+++ DreamJob/src/test/java/project/AppTest.java
@@ -1,6 +1,11 @@
 package project;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 /**
  * Unit test for simple App.
@@ -11,8 +16,26 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void AssertionChecking()
     {
-       System.out.println("print");
+    	WebDriver driver = new ChromeDriver();
+    	
+    	driver.get("https://www.amazon.in");
+    	
+       
+       // Using verify (in TestNG or with soft assertions)
+          SoftAssert softAssert = new SoftAssert();
+          System.out.println(driver.getTitle());
+          softAssert.assertEquals(driver.getTitle(), "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+
+          // Continue with other steps even if the above verification fails
+          softAssert.assertTrue(driver.findElement(By.id("twotabsearchtextbox")).isDisplayed());
+
+          // Collect all verification results
+          softAssert.assertAll();
+           Assert.assertEquals(driver.getTitle(), "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+
+          driver.quit();
     }
+ 
 }
